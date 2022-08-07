672a0ac5f07f5c62753114ffaa696ff2dc0e999f
masyan455 <109448816+masyan455@users.noreply.github.com>
2022-08-07 14:32:03 +0700

    โปรแกรมลบเลข

---
+++ โปรแกรมบวกเลข 22/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c As Single
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = a - b
+        TextBox3.Text = c
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) 
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+End Class
